2517102d61fae13d6bb6d836dcc0b781e454d076
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2020-03-31 16:19:56 +0400

    Ajout de swagger pour la documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const options = new DocumentBuilder()
+    .setTitle('Suivid19 API')
+    .setDescription('Description de l\'API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document, {
+    //customfavIcon: '',
+    customSiteTitle: 'Suivid19 API'
+  });
+
   await app.listen(3000);
 }
 bootstrap();
