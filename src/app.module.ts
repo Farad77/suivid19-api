d42334daf87d2a07ce2a3ef173270f453b6ca36c
Ibalien <julienpad467@gmail.com>
2020-04-01 17:35:35 +0400

    Tests class

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { Connection } from 'typeorm';
+import { TestsModule } from './tests/tests.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    UsersModule
+    UsersModule,
+    TestsModule
   ],
   controllers: [AppController],
   providers: [AppService],
