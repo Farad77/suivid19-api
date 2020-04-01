0b661b509a0b56ecb8cee784adfefcc23d6a3a6b
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2020-04-01 15:48:45 +0400

    ajout des repository dans l'import des modules Person et Relative

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PersonsController } from './persons.controller';
 import { PersonsService } from './persons.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './persons.entity';
+import { PersonRepository } from './persons.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Person, PersonRepository])],
+  exports: [TypeOrmModule],
   controllers: [PersonsController],
   providers: [PersonsService]
 })
--- src/relatives/relatives.module.ts
+++ src/relatives/relatives.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RelativesController } from './relatives.controller';
 import { RelativesService } from './relatives.service';
+import { Relative } from './relatives.entity';
+import { RelativeRepository } from './relatives.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Relative, RelativeRepository])],
+  exports: [TypeOrmModule],
   controllers: [RelativesController],
   providers: [RelativesService]
 })
