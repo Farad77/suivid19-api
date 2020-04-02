451996f085d7cfb3b9bcb41b7e5d03b3b38df61f
Ibalien <julienpad467@gmail.com>
2020-04-02 15:00:38 +0400

    Update symptoms.service.spec.ts

--- src/symptoms/symptoms.service.spec.ts
+++ src/symptoms/symptoms.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SypmtomsService } from './sypmtoms.service';
+import { SypmtomsService } from './symptoms.service';
 
 describe('SypmtomsService', () => {
   let service: SypmtomsService;
