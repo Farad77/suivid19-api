5689427b7daad3288ab857ecdf326e3df196c222
zygof <46953622+zygof@users.noreply.github.com>
2020-04-02 13:52:15 +0400

    add import + export module notif + tracking

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './notifications.entity';
+import { NotificationRepository } from './notifications.repository';
 import { NotificationsService } from './notifications.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notification, NotificationRepository])],
+  exports: [TypeOrmModule],
   controllers: [NotificationsController],
   providers: [NotificationsService]
 })
--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrackingController } from './tracking.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tracking } from './tracking.entity';
+import { TrackingRepository } from './tracking.repository';
 import { TrackingService } from './tracking.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tracking, TrackingRepository])],
+  exports: [TypeOrmModule],
   controllers: [TrackingController],
   providers: [TrackingService]
 })
