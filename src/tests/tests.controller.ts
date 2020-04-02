c43ff0f2248e89421db1e44f4999e4c06df425bf
Ibalien <julienpad467@gmail.com>
2020-04-02 08:52:40 +0400

    Update tests.controller.ts

--- src/tests/tests.controller.ts
+++ src/tests/tests.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { Test } from './tests.entity';
 import { TestsService } from './tests.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tests')
 @Controller('tests')
 export class TestsController {
 
