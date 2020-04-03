3c6c25551094ca5d945ce17eff068da97c95291e
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2020-04-03 09:16:17 +0400

    ajout de la génération du token JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, UseGuards, Post, Request, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
   
   @ApiTags('authentification')
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
